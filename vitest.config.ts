c3371f539504a55e4139a5b191922690cdffaf65
Jan Macku <jamacku@redhat.com>
2026-04-28 15:44:23 +0200

    test: update vitest config

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { configDefaults, defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    dir: './test',
+
+    exclude: [...configDefaults.exclude],
+
+    coverage: {
+      include: ['src/**/*.ts'],
+    },
+  },
+});
